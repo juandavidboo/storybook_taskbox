20cfe3cac954cfd3804b67f1313e1c9bd5af09f2
Juan David Hernandez <juandavid.hernandez@juandav-ltm9m4t.internal.salesforce.com>
2021-11-25 17:54:01 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,41 @@
+//import logo from './logo.svg';
+//import './App.css';
+//
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+
+//export default App;
+
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+
+import InboxScreen from './components/InboxScreen';
+
+import './index.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+  );
+}
+export default App;
